e81116f3b6b8daf02e9d12b27255facb3ec4fe82
rodrigogivisiez <rodrigo2017@gmail.com>
2018-07-01 20:19:41 -0300

    add construtor in Givix class

--- app/src/main/java/com/base/mybiblioteca/Givix.java
+++ app/src/main/java/com/base/mybiblioteca/Givix.java
@@ -6,6 +6,9 @@ import android.widget.Toast;
 public class Givix {
 
 
+    public Givix() {
+    }
+
     public void myToast(Context context){
 
         Toast.makeText(context, "teste my lib", Toast.LENGTH_SHORT).show();
